344d611c9570a535dbf4aaa875c5700bd497ba98
Peter van Bijsterveldt <petervbijsterveldt@gmail.com>
2017-02-19 20:07:48 +0100

    Entry point and canvas class

---
+++ src/core/DrawArea.java
@@ -0,0 +1,16 @@
+package core;
+
+import javax.swing.*;
+import java.awt.*;
+
+class DrawArea extends JPanel {
+
+    public DrawArea() {
+
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+
+    }
+}
---
+++ src/core/Main.java
@@ -0,0 +1,19 @@
+package core;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JFrame {
+
+    public static void main(String[] args) {
+        new Main();
+    }
+
+    private Main() throws HeadlessException {
+        setSize(600, 400);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setContentPane(new DrawArea());
+        setVisible(true);
+    }
+}
